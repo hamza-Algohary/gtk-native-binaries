a4f752e905eb14900c76a3556d3dfff1b4519bbf
hamza-Algohary <hamza.moza.mizo@gmail.com>
2024-08-20 16:45:52 +0300

    Nothing.

--- install.sh
+++ install.sh
@@ -9,4 +9,3 @@ make
 make install
 export PATH=$CWD:$PATH 
 python3 msys.py jar mingw-w64-x86_64-gtk4 mingw-w64-x86_64-libadwaita mingw-w64-x86_64-gstreamer --name gtk_native.jar --class GtkNative --package gtk_native --path win --output lib
-mvn install:install-file -Dfile=lib/gtk_native.jar -DgroupId="io.github.hamza-Algohary" -DartifactId=gtk_native -Dversion=0.1.0 -Dpackaging=jar -DgeneratePom=true
