ba7826e96853daa3adf82caeb1058c42d0421aee
hamza-Algohary <hamza.moza.mizo@gmail.com>
2024-08-22 08:18:51 +0300

    Fixed the prefix.

--- install.sh
+++ install.sh
@@ -3,7 +3,7 @@ wget https://www.python.org/ftp/python/3.12.5/Python-3.12.5.tgz
 tar xzf Python-3.12.5.tgz
 cd Python-3.12.5
 
-./configure --prefix=python/3.12.5/ --enable-optimizations --with-lto --with-computed-gotos --with-system-ffi
+./configure --prefix=$PWD/python/3.12.5/ --enable-optimizations --with-lto --with-computed-gotos --with-system-ffi
 make -j "$(nproc)"
 make altinstall
 python/3.12.5/python3 msys.py jar mingw-w64-x86_64-gtk4 mingw-w64-x86_64-libadwaita mingw-w64-x86_64-gstreamer --name gtk_native.jar --class GtkNative --package gtk_native --path win --output lib
