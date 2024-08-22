4cc06185f8ddd753565785608a9721de8ff81ad4
hamza-Algohary <hamza.moza.mizo@gmail.com>
2024-08-22 08:10:14 +0300

    Nothing.

--- install.sh
+++ install.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
-wget https://www.python.org/ftp/python/3.6.10/Python-3.6.10.tgz
-tar zxfv Python-3.6.10.tgz
-rm Python-3.6.10.tgz
-find ./Python-3.6.10/Python -type d | xargs chmod 0755
-cd Python-3.6.10
-./configure --prefix=$PWD/Python-3.6.10/Python
-make
-make install
-export PATH=$CWD:$PATH 
-python3 msys.py jar mingw-w64-x86_64-gtk4 mingw-w64-x86_64-libadwaita mingw-w64-x86_64-gstreamer --name gtk_native.jar --class GtkNative --package gtk_native --path win --output lib
+cd /tmp/
+wget https://www.python.org/ftp/python/3.12.5/Python-3.12.5.tgz
+tar xzf Python-3.12.5.tgz
+cd Python-3.12.5
+
+sudo ./configure --prefix=/opt/python/3.12.5/ --enable-optimizations --with-lto --with-computed-gotos --with-system-ffi
+sudo make -j "$(nproc)"
+sudo make altinstall
+sudo rm /tmp/Python-3.12.5.tgz 
+/opt/python/3.12.5/python3 msys.py jar mingw-w64-x86_64-gtk4 mingw-w64-x86_64-libadwaita mingw-w64-x86_64-gstreamer --name gtk_native.jar --class GtkNative --package gtk_native --path win --output lib
